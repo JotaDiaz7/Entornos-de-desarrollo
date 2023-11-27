aab9792f93e8f5593cf0c06267ad19b318a06364
Alicia Ubeda <aliubemor@alu.edu.gva.es>
2023-11-27 21:50:29 +0100

    Cambios Alicia Ubeda

--- src/actividadUT5/Actividad.java
+++ src/actividadUT5/Actividad.java
@@ -3,5 +3,7 @@ package actividadUT5;
 public class Actividad {
     public static void main(String[] args){
         System.out.println("Hola soy Jota");
+
+        System.out.println("Hola soy Alicia");
     }
 }
